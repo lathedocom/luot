3a9ff5c7e2cd5b291231784b3e2b8701b34ca709
lathedocom <lathedo.taichi@gmail.com>
2026-07-20 23:57:36 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
     initSearch();
     renderSkeletons();
     fetchNewsData();
+    fetchTimelineData();
 });
  
 function initNavigation() {
