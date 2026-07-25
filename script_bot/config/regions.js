337db095bfbf5affce8aca54eba24cca4d970c3e
lathedocom <lathedo.taichi@gmail.com>
2026-07-25 15:59:57 +0700

    Update regions.js

--- script_bot/config/regions.js
+++ script_bot/config/regions.js
@@ -1,24 +1,58 @@
-// Khai báo các khu vực (Region) và quy tắc Map nguồn mặc định
+// FILE: script_bot/config/regions.js
 
 module.exports = {
     REGIONS: [
-        { id: 'vietnam', name: 'Việt Nam', keywords: ['việt nam', 'hà nội', 'tphcm', 'chính phủ vn', 'thủ tướng', 'vnindex'] },
-        { id: 'usa', name: 'Mỹ', keywords: ['mỹ', 'hoa kỳ', 'washington', 'biden', 'fed', 'wall street', 'new york', 'trump'] },
-        { id: 'china', name: 'Trung Quốc', keywords: ['trung quốc', 'bắc kinh', 'tập cận bình', 'đài loan', 'thượng hải'] },
-        { id: 'eu', name: 'Châu Âu', keywords: ['châu âu', 'eu', 'liên minh châu âu', 'anh', 'pháp', 'đức', 'ecb'] },
-        { id: 'asean', name: 'Đông Nam Á', keywords: ['asean', 'đông nam á', 'thái lan', 'indonesia', 'malaysia', 'singapore'] },
-        { id: 'global', name: 'Toàn cầu', keywords: ['toàn cầu', 'thế giới', 'wto', 'who', 'world bank'] }
+        { 
+            id: 'vietnam', 
+            name: 'Việt Nam', 
+            keywords: ['việt nam', 'hà nội', 'tphcm', 'chính phủ', 'thủ tướng chính phủ', 'vnindex', 'nhà nước'] 
+        },
+        { 
+            id: 'usa', 
+            name: 'Mỹ', 
+            keywords: ['mỹ', 'hoa kỳ', 'washington', 'biden', 'fed', 'wall street', 'new york', 'trump', 'lầu năm góc'] 
+        },
+        { 
+            id: 'china', 
+            name: 'Trung Quốc', 
+            keywords: ['trung quốc', 'bắc kinh', 'tập cận bình', 'đài loan', 'thượng hải', 'hong kong'] 
+        },
+        { 
+            id: 'eu', 
+            name: 'Châu Âu', 
+            keywords: ['châu âu', 'eu', 'liên minh châu âu', 'anh', 'pháp', 'đức', 'ecb', 'ukraine', 'nga', 'moscow', 'kyiv', 'putin', 'zelensky'] 
+        },
+        { 
+            id: 'asean', 
+            name: 'Đông Nam Á', 
+            keywords: ['asean', 'đông nam á', 'thái lan', 'indonesia', 'malaysia', 'singapore', 'philippines', 'kuala lumpur'] 
+        },
+        { 
+            id: 'asia', 
+            name: 'Châu Á', 
+            keywords: ['châu á', 'nhật bản', 'hàn quốc', 'ấn độ', 'tokyo', 'seoul', 'new delhi', 'takaichi', 'yoon suk yeol', 'modi'] 
+        },
+        { 
+            id: 'middle_east', 
+            name: 'Trung Đông', 
+            keywords: ['trung đông', 'israel', 'iran', 'palestine', 'gaza', 'hamas', 'syria', 'lebanon', 'hezbollah'] 
+        },
+        { 
+            id: 'global', 
+            name: 'Toàn cầu', 
+            keywords: ['toàn cầu', 'thế giới', 'wto', 'who', 'world bank', 'imf', 'liên hợp quốc', 'nato'] 
+        }
     ],
     
-    // Gán nhãn cứng khu vực dựa trên tên nguồn báo (Rule-based siêu tốc)
+    // Gán nhãn cứng khu vực dựa trên tên nguồn báo
+    // CHỈ áp dụng cho các báo có tính chuyên biệt vùng miền rõ rệt.
     SOURCE_DEFAULT_REGION: {
-        'VNExpress': 'vietnam',
-        'VnEconomy': 'vietnam',
         'Reuters': 'global',
         'CNBC': 'usa',
         'BBC News': 'global',
         'The Wall Street Journal': 'usa',
         'The Economist': 'global',
-        'Al Jazeera': 'global'
+        'Al Jazeera': 'middle_east'
+        // Đã xóa VNExpress, VnEconomy... khỏi đây để buộc hệ thống phải đọc hiểu từ khóa thay vì gán mù quáng.
     }
 };
